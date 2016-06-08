b2dd259287425bff05acd6fc9d93319c6b5453b3
Lirian Su <liriansu@gmail.com>
2016-06-08 14:11:01 +0000

    Improve url parsing logic in server.py

--- wescraper/server.py
+++ wescraper/server.py
@@ -9,24 +9,23 @@ dirname = os.path.dirname(os.path.realpath(sys.argv[0]))
 scraper = os.path.join(dirname,  "scraper.py")
 
 class WeHandler(tw.RequestHandler):
-    def get(self, key_type, path):
-        if not key_type in config.types:
-            self.write("Type should be {}".format(str(config.types)))
-            return
-        if path:
+    def get(self, key_type, path=None):
+        if key_type in config.types and path:
             accounts = path.split('/')
             p = subprocess.Popen(["python", scraper, key_type] + accounts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
             print str(err)
             self.write(out)
         else:
-            self.write("You may specify a URL to search, these urls are valid:\n")
+            self.write("Change url to http://host/type/key1/key2 to search.\n")
+            self.write("For example, these urls are valid:\n")
             for t in config.types:
-                self.write("http://host/{}/liriansu/miawu".format(t))
+                self.write("http://host/{}/liriansu\n".format(t))
 
 app = tw.Application([
     (r'/(favicon.ico)', tw.StaticFileHandler, {"path": ""}),
-    (ur'/(.*)/(.*)', WeHandler)
+    (ur'/(.*)/(.*)', WeHandler),
+    (ur'/(.*)', WeHandler)
 ])
 app.listen(config.tornado_port)
 print "Server is up on port {} now.".format(config.tornado_port)
